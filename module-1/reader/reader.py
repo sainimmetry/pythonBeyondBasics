de136d5ca3b37e4080ea97a73540adf11c9c0699
Sai Pavan <saipavan@nimmetry.com>
2019-04-03 17:12:44 +0530

    packages in Python

--- module-1/reader/reader.py
+++ module-1/reader/reader.py
@@ -1,5 +1,15 @@
+import os
+from reader.compressed import bzipped,gzipped
+
+extension_map ={
+    'bz2': bzipped.opener,
+    'gz2' : gzipped.opener
+}
+
 class Reader:
     def __init__(self,filename):
+        extension = os.path.splitext(filename)[1]
+        opener = extension_map.get(extension,open)
         self.filename = filename
         self.f = open(self.filename,'rt')
 
